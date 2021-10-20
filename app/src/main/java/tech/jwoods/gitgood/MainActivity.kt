30a8458e89323425f2f6fc2f29a91ea5dd3decdd
Jessica <Jessica@jwoods.tech>
2021-10-20 21:12:31 +1100

    Alternate theme per row

--- app/src/main/java/tech/jwoods/gitgood/MainActivity.kt
+++ app/src/main/java/tech/jwoods/gitgood/MainActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.width
@@ -59,31 +60,14 @@ fun Title() {
 @Composable
 fun MainActivityView(repos: List<GithubRepo>) {
     Title()
-    val dateFormat = SimpleDateFormat("dd/MM/yyyy")
-    GitGoodTheme {
-        Surface(color = MaterialTheme.colors.background) {
-            LazyColumn {
-                var i = 0
-                item { Title() }
-                items(repos) { repo ->
-                    Row {
-                        Text(text = "$i")
-                        Spacer(modifier = Modifier.width(12.dp))
-                        Text(
-                            text = dateFormat.format(repo.date),
-                            color = MaterialTheme.colors.secondaryVariant,
-                            style = MaterialTheme.typography.subtitle2,
-                        )
-                        Spacer(modifier = Modifier.width(12.dp))
-                        Text(
-                            text = repo.name,
-                            color = MaterialTheme.colors.primary,
-                            style = MaterialTheme.typography.body1,
-                        )
-
-                    }
-                    i++
-                }
+    Surface(color = MaterialTheme.colors.background) {
+        LazyColumn {
+            var i = 0
+            item { Title() }
+            items(repos) { repo ->
+                if (i % 2 == 0) GitGoodTheme() { Row(repo, i) }
+                else Row(repo, i)
+                i++
             }
         }
     }
@@ -99,4 +83,28 @@ fun DefaultPreview() {
         GithubRepo("Cool Repo 2", date),
     )
     MainActivityView(repos)
+}
+
+@Composable
+fun Row(repo: GithubRepo, count: Int) {
+    val dateFormat = SimpleDateFormat("dd/MM/yyyy")
+    Row(modifier = Modifier.clickable(onClick = ::onRowClicked)) {
+        Text(text = "$count")
+        Spacer(modifier = Modifier.width(12.dp))
+        Text(
+            text = dateFormat.format(repo.date),
+            color = MaterialTheme.colors.secondaryVariant,
+            style = MaterialTheme.typography.subtitle2,
+        )
+        Spacer(modifier = Modifier.width(12.dp))
+        Text(
+            text = repo.name,
+            color = MaterialTheme.colors.primary,
+            style = MaterialTheme.typography.body1,
+        )
+    }
+}
+
+fun onRowClicked() {
+
 }
